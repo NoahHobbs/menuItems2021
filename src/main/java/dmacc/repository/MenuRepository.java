27918c13ffcf76cea3ec792283a2f0f3f4d445ea
Noah Hobbs <mlzgphoenix@gmail.com>
2021-03-18 17:48:44 -0500

    Changes made to menuRepo

--- src/main/java/dmacc/repository/MenuRepository.java
+++ src/main/java/dmacc/repository/MenuRepository.java
@@ -1,5 +1,6 @@
 package dmacc.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,7 @@ import dmacc.beans.Menu;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long>{
-
+	
 }
+
+
